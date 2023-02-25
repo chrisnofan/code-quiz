d43cc97267af7f07ff40752594c4a4d3afe8631e
chrisnofan <chrisnofan@gmail.com>
2023-02-25 22:40:27 +0100

    created the quiz questions in questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,45 @@
+//declaring the quiz questions
+var quizQuestion = [
+    //question 1
+    {
+    question: "Commonly used data types DO NOT include: ", 
+    answer: ["Strings", "Booleans", "Alerts", "Numbers"],
+    correct: "Alerts",
+    },
+
+    //question 2
+    {
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    answers: ["var", "let", "Both A and B", "None of the above"],
+    correct: "Both A and B",
+    },
+
+    //question 3
+    {
+    question: "The condition in an if / else statement is enclosed within ____.", 
+    answer: ["Quotes", "Curly brackets", "Parentheses", "Square brackets"],
+    correct: "Parenthesis",
+    },
+
+    //question 4
+    {
+    question: "Arrays in JavaScript can be used to store ____.", 
+    answer: ["Numbers and Strings", "Other arrays", "Booleans", "All of the above"],
+    correct: "All of the above",
+    },
+    
+    //question 5
+    {
+    question: "String values must be enclosed within _____ when being assigned to variables.", 
+    answer: ["Commas", "Curly brackets", "Quotes", "Parentheses"],
+    correct: "Quotes",
+    },
+
+    //question 6
+    {
+    question: "Which function is used to serialize an object into a JSON string in Javascript?",
+    answers: ["stringify()", "parse()", "convert()", "None of the above"],
+    correct: "stringify()",
+    }
+
+];
